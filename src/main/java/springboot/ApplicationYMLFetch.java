352f1afccde4b085e83339477e8e73959e516342
Raghu Kiran <raghu.kiran@elevancehealth.com>
2025-04-22 09:57:28 -0400

    Application.yml

---
+++ src/main/java/springboot/ApplicationYMLFetch.java
@@ -0,0 +1,26 @@
+package springboot;
+
+public class ApplicationYMLFetch {
+
+/*
+    @Configuration
+    @EnableConfigurationProperties
+    @ConfigurationProperties
+    public class ApplicationConfiguration {
+        // Configuration Bean Class attributes and methods...
+    }
+
+    @RestController
+    @EnableAsync
+    @SpringbootApplication
+    @PeopertySource("classpath:application.yml")
+    public class ActuatorApplication {
+        // Main class or Controller class attributes and methods...
+    }
+
+*/
+
+    public static void main(String[] args) {
+        System.out.println("How to enable application or class to read properties from application.yml file");
+    }
+}
